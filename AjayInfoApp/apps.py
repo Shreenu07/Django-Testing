237819607f3a43a0667fb50e56c30664aee2b9a4
Shreenu07 <Sriniwas.ajayinfo@gmail.com>
2024-08-12 14:10:04 +0530

    first commit

---
+++ AjayInfoApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AjayinfoappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AjayInfoApp'
---
+++ AjayInfoApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# home view:
+
+def home(Request):
+    return render(Request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def products(request):
+    return render(request,'products.html')
